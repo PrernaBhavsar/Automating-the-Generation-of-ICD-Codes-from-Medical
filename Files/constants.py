7b0096d8a9b99809a1d4bd8589b3e5324134b256
Parth Jhunjhunwala <pjjw25@gmail.com>
2020-05-13 11:54:11 +0530

    Add files via upload

---
+++ Files/constants.py
@@ -0,0 +1,10 @@
+PAD_CHAR = "**PAD**"
+EMBEDDING_SIZE = 100
+MAX_LENGTH = 2500
+
+#where you want to save any models you may train
+MODEL_DIR = r'/path/to/repo/saved_models/'
+
+DATA_DIR = '/content/gdrive/My Drive/BE Project/CLSTM/Data'
+MIMIC_3_DIR = '/content/gdrive/My Drive/BE Project/CLSTM/Data'
+MIMIC_2_DIR = r'C:\Users\admin\Desktop\BE Project\caml-mimic-master\mimicdata\mimic2'
